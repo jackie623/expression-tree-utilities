06ecb432282c626cb40fa672c3e94d86da3772b6
Barry Wark <barrywark@gmail.com>
2011-12-20 01:26:06 -0500

    Adding project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject expression-tree-utils "1.1"
+  :description "ExpressionTree utilities"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
+                 "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"}
+  :aot [expression-tree-utils.core])
