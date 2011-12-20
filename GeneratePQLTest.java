fc515ae6f6669ce684489f4a52a985b8247c74c8
Barry Wark <barrywark@gmail.com>
2011-12-20 14:22:51 -0500

    Working Java interop example

--- GeneratePQLTest.java
+++ GeneratePQLTest.java
@@ -9,10 +9,24 @@ public class GeneratePQLTest {
     public static void main(String[] args)
     {
 
+        java.lang.Thread.currentThread().setContextClassLoader(GeneratePQLTest.class.getClassLoader()); 
+        
         ExpressionBuilder.ReturnValue rv = core.EditExpressionTree(null);
         ExpressionTree tree = rv.expressionTree;
         System.out.println(core.generatePQL(
             tree.getRootExpression()
         ));
+        System.exit(0);
+    }
+    
+    public static String edit()
+    {
+        java.lang.Thread.currentThread().setContextClassLoader(GeneratePQLTest.class.getClassLoader()); 
+        
+        ExpressionBuilder.ReturnValue rv = core.EditExpressionTree(null);
+        ExpressionTree tree = rv.expressionTree;
+        
+        return core.generatePQL(tree.getRootExpression());
+        
     }
 }
