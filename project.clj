20abec78b275638f306e16a18c48dc377a6661f2
Barry Wark <barrywark@gmail.com>
2011-12-21 11:43:04 -0500

    Lein deps

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject expression-tree-utils "1.1"
   :description "ExpressionTree utilities"
 
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
 
-  :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]
+  :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]
                      [lein-autotest "1.1.0"]
                      [lein-play "1.0.0-SNAPSHOT"]
                      [lein-test-out "0.1.0"]]
